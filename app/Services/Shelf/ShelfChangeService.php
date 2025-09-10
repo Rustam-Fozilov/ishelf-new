d1590b04610db140ed0871277624596b4596ea60
Rustam-Fozilov <thisisrustam@gmail.com>
2025-09-10 15:01:32 +0500

    ordering/product route boshlandi

--- app/Services/Shelf/ShelfChangeService.php
+++ app/Services/Shelf/ShelfChangeService.php
@@ -2,7 +2,10 @@
 
 namespace App\Services\Shelf;
 
+use App\Models\Shelf\ProductShelfTemp;
+use App\Models\Shelf\Shelf;
 use App\Models\Shelf\ShelfChange;
+use App\Models\Shelf\ShelfChangeItem;
 
 class ShelfChangeService
 {
@@ -12,5 +15,29 @@ class ShelfChangeService
             ->where('shelf_id', $shelf_id)
             ->orderBy('id', 'desc')
             ->first();
+    }
+
+    public static function create(Shelf $shelf, int $user_id = null): ShelfChange
+    {
+        $change = ShelfChange::query()->create([
+            'user_id'      => $user_id ?? auth()->id(),
+            'shelf_id'     => $shelf->id,
+            'category_sku' => $shelf->category_sku,
+        ]);
+
+        $shelfTemp = ProductShelfTemp::query()->whereNotNull('sku')->where('shelf_id', $shelf->id)->get();
+        foreach ($shelfTemp as $temp) {
+            ShelfChangeItem::query()->create([
+                'sku'             => $temp->sku,
+                'place'           => $temp->place,
+                'floor'           => $temp->floor,
+                'shelf_id'        => $shelf->id,
+                'ordering'        => $temp->ordering,
+                'floor_ordering'  => $temp->floor_ordering,
+                'shelf_change_id' => $change->id,
+            ]);
+        }
+
+        return $change;
     }
 }
